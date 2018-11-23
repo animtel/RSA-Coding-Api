f059f049ada44c5a55903436b84aab9f584bd2f3
Daniel Kovalenko <D.Kovalenko@total.one>
2018-11-23 21:10:03 +0200

    [added] - decode rules

--- RSA-Coding-Api/Controllers/ValuesController.cs
+++ RSA-Coding-Api/Controllers/ValuesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,36 +11,55 @@ namespace RSA_Coding_Api.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly long d = 179;
+        private readonly long n = 209;
+
+        [HttpPost, Route("decodeMsg")]
+        public ActionResult<string> decodeMessg([FromBody]string code)
+            => decode(code.Split(" ").AsEnumerable(), d, n);
+
+        public Func<IEnumerable<string>, long, long, string> decode = (msgCollection, d, n) =>
+            String.Concat(msgCollection
+                .Select(msg => 
+                characters[
+                    Convert.ToInt32(
+                        (BigInteger.Pow(
+                            new BigInteger(
+                                Convert.ToDouble(msg)
+                                ),(int)d
+                            ) % new BigInteger((int)n)).ToString()
+                        )
+                    ].ToString()
+                )
+                );
+
+        private string RSA_Dedoce(List<string> input, long d, long n)
         {
-            return new string[] { "value1", "value2" };
+            string result = "";
+
+            BigInteger bi;
+
+            foreach (string item in input)
+            {
+                bi = new BigInteger(Convert.ToDouble(item));
+                bi = BigInteger.Pow(bi, (int)d);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                int index = Convert.ToInt32(bi.ToString());
+
+                result += characters[index].ToString();
+            }
+
+            return result;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        static char[] characters = new char[] { '#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                '8', '9', '0' };
     }
 }
